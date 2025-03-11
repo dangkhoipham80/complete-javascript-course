e17ecf1de828070a7fb388cd752c94a74cff162d
Phạm Đăng Khôi <dangkhoipham80@gmail.com>
2025-03-11 15:50:17 +0700

    array + assignment 6

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -73,18 +73,87 @@
 
 // Assgiment 5
 /* Write your code below. Good luck! 🙂 */
-const calcAverage = (first, second, third) => (first + second + third) / 3;
+// const calcAverage = (first, second, third) => (first + second + third) / 3;
 
-const checkWinner = (avgDolphins, avgKoalas) => {
-    if (avgDolphins >= 2 * avgKoalas) 
-        console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`)
-    else if(avgKoalas >= 2 * avgDolphins) 
-        console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`)
-    else
-        console.log("No team wins...")
-}
+// const checkWinner = (avgDolphins, avgKoalas) => {
+//     if (avgDolphins >= 2 * avgKoalas) 
+//         console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`)
+//     else if(avgKoalas >= 2 * avgDolphins) 
+//         console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`)
+//     else
+//         console.log("No team wins...")
+// }
 
-const scoreDolphins  = calcAverage(44, 23, 71);
-const scoreKoalas = calcAverage(65, 54, 49);
+// const scoreDolphins  = calcAverage(44, 23, 71);
+// const scoreKoalas = calcAverage(65, 54, 49);
 
-checkWinner(scoreDolphins, scoreKoalas)
+// checkWinner(scoreDolphins, scoreKoalas)
+
+// 39. Introduction to Arrays
+const friend1 = 'Michael';
+const friend2 = 'Steven';    
+const friend3 = 'Peter';
+
+const friends = ['Michael', 'Steven', 'Peter'];
+console.log(friends);
+
+const years = new Array(1991, 1984, 2008, 2020);
+console.log(years);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = 'Jay';
+console.log(friends);
+
+const firstName = 'Jonas';
+const jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends];
+console.log(jonas);
+console.log(jonas.length);
+
+// 40. Basic Array Operations (Methods)
+const newLength = friends.push('Khôi');
+console.log(friends);
+console.log(newLength);
+
+const a = friends;
+a.push('Khoa');
+console.log(a)
+console.log(friends)
+
+friends.unshift('John');
+console.log(friends);
+
+const popped = friends.pop();
+console.log(popped);
+console.log(friends)
+
+friends.shift();
+console.log(friends);
+
+console.log(friends.indexOf('Steven'));
+console.log(friends.indexOf('Bob'));
+
+console.log(friends.includes('Steven'));
+console.log(friends.includes('Bob'));
+
+friends.push(23)
+console.log(friends);
+console.log(friends.includes('23'));
+console.log(friends.includes(23));
+
+// Assignment
+const calcTip = (bill) => bill > 50 && bill < 300 ? 0.15 * bill : 0.2 * bill;
+
+const bills = [125, 555, 44];
+
+const tips = bills.map(calcTip);
+
+const totals = bills.map((bill, index) => bill + tips[index]);
+
+bills.forEach((bill, index) => {
+    console.log(`Bill: $${bill}, Tip: $${tips[index]}, Total: $${totals[index]}`);
+});
