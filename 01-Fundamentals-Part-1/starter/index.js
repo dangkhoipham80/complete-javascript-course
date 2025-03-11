35546bd609c6c4070963546d79acd8889280a83e
Phạm Đăng Khôi <dangkhoipham80@gmail.com>
2025-03-11 14:53:52 +0700

    01

---
+++ 01-Fundamentals-Part-1/starter/index.js
@@ -0,0 +1,34 @@
+// let massMark = parseFloat(prompt("massMark:"))
+// let heightMark = parseFloat(prompt("heightMark:"))
+// let massJohn = parseFloat(prompt("massJohn:"))
+// let heightJohn = parseFloat(prompt("heightJohn:"))
+
+// let BMIMark = massMark / (heightMark ** 2)
+// let BMIJohn = massJohn / (heightJohn ** 2)
+
+// console.log(BMIMark)
+// console.log(BMIJohn)
+
+// let markHigherBMI  = heightMark > heightJohn
+// console.log(markHigherBMI)
+
+// const firstName = "Jonas"
+// const lastName = "ABCD"
+// const sayHi = `Hello ${firstName} ${lastName}`
+
+// console.log(sayHi)
+// console.log(`This is the
+// sayHi:
+// ${sayHi}`)
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+
+/* Write your code below. Good luck! 🙂 */
+
