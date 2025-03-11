30c1d4a7959b3ce4202cc07b5ce1ac9a8daa8039
Phạm Đăng Khôi <dangkhoipham80@gmail.com>
2025-03-11 16:06:11 +0700

    object

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -90,70 +90,90 @@
 // checkWinner(scoreDolphins, scoreKoalas)
 
 // 39. Introduction to Arrays
-const friend1 = 'Michael';
-const friend2 = 'Steven';    
-const friend3 = 'Peter';
+// const friend1 = 'Michael';
+// const friend2 = 'Steven';    
+// const friend3 = 'Peter';
 
-const friends = ['Michael', 'Steven', 'Peter'];
-console.log(friends);
+// const friends = ['Michael', 'Steven', 'Peter'];
+// console.log(friends);
 
-const years = new Array(1991, 1984, 2008, 2020);
-console.log(years);
+// const years = new Array(1991, 1984, 2008, 2020);
+// console.log(years);
 
-console.log(friends[0]);
-console.log(friends[2]);
+// console.log(friends[0]);
+// console.log(friends[2]);
 
-console.log(friends.length);
-console.log(friends[friends.length - 1]);
+// console.log(friends.length);
+// console.log(friends[friends.length - 1]);
 
-friends[2] = 'Jay';
-console.log(friends);
+// friends[2] = 'Jay';
+// console.log(friends);
 
-const firstName = 'Jonas';
-const jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends];
-console.log(jonas);
-console.log(jonas.length);
+// const firstName = 'Jonas';
+// const jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends];
+// console.log(jonas);
+// console.log(jonas.length);
 
-// 40. Basic Array Operations (Methods)
-const newLength = friends.push('Khôi');
-console.log(friends);
-console.log(newLength);
+// // 40. Basic Array Operations (Methods)
+// const newLength = friends.push('Khôi');
+// console.log(friends);
+// console.log(newLength);
 
-const a = friends;
-a.push('Khoa');
-console.log(a)
-console.log(friends)
+// const a = friends;
+// a.push('Khoa');
+// console.log(a)
+// console.log(friends)
 
-friends.unshift('John');
-console.log(friends);
+// friends.unshift('John');
+// console.log(friends);
 
-const popped = friends.pop();
-console.log(popped);
-console.log(friends)
+// const popped = friends.pop();
+// console.log(popped);
+// console.log(friends)
 
-friends.shift();
-console.log(friends);
+// friends.shift();
+// console.log(friends);
 
-console.log(friends.indexOf('Steven'));
-console.log(friends.indexOf('Bob'));
+// console.log(friends.indexOf('Steven'));
+// console.log(friends.indexOf('Bob'));
 
-console.log(friends.includes('Steven'));
-console.log(friends.includes('Bob'));
+// console.log(friends.includes('Steven'));
+// console.log(friends.includes('Bob'));
 
-friends.push(23)
-console.log(friends);
-console.log(friends.includes('23'));
-console.log(friends.includes(23));
+// friends.push(23)
+// console.log(friends);
+// console.log(friends.includes('23'));
+// console.log(friends.includes(23));
 
-// Assignment
-const calcTip = (bill) => bill > 50 && bill < 300 ? 0.15 * bill : 0.2 * bill;
+// // Assignment
+// const calcTip = (bill) => bill > 50 && bill < 300 ? 0.15 * bill : 0.2 * bill;
 
-const bills = [125, 555, 44];
+// const bills = [125, 555, 44];
 
-const tips = bills.map(calcTip);
+// const tips = bills.map(calcTip);
 
-const totals = bills.map((bill, index) => bill + tips[index]);
+// const totals = bills.map((bill, index) => bill + tips[index]);
 
-bills.forEach((bill, index) => {
-    console.log(`Bill: $${bill}, Tip: $${tips[index]}, Total: $${totals[index]}`);
-});
+// bills.forEach((bill, index) => {
+//     console.log(`Bill: $${bill}, Tip: $${tips[index]}, Total: $${totals[index]}`);
+// });
+
+// 42. Introduction to Object
+const jonasArray = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven']
+]
+console.log(jonasArray)
+
+const jonasObject = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    age: 2037 - 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven']
+}
+
+console.log(jonasObject)
